2964a6d153c6fbd26d41a3e9c3df8c91088b8d29
marcolanaro <marcolanaro@gmail.com>
2013-11-24 18:02:47 +0100

    Redis-Scale

---
+++ Redis-Scale/srv2/app2.js
@@ -0,0 +1,20 @@
+var options = {
+	nameSpace: "dataChannel",
+	redis: {host: "localhost", port: 6380},
+	cookie: {name: "datachannel.io", secret: "thisismysecretkey"}
+};
+
+
+var server = require('http').createServer()
+  , connect = require('connect')
+  , RedisStore = require("connect-redis")(connect)
+  , redis = require("redis");
+
+
+var dc = require('dataChannel.io').listen(server, {
+	nameSpace: options.nameSpace,
+	redis: {port: options.redis.port, host: options.redis.host, options: {}},
+	static: true
+});
+
+server.listen(8082);
